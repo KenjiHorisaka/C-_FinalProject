184ba7b812c62a1910ba2df3f41bc75dcc925470
ronaldcumbal <ronald.cumbal.g@gmail.com>
2017-05-04 18:03:03 +0200

    Header files and a Test file
    Las clases Matrix, Vector, Heat1D y Heat2D estan compiladas como header files. En el archivo TestFile las pueden probar o solo las deben incluir en sus propios programas (#include Matrix.h", etc..). La clase Vector y Matrix imprimer los objetos de forma estructurada y sin los zeros. Esta parcialmente implementado la funcion Vector<double> exact(double t) const de las clases HeatD, aun tengo que revisarla. Ninguna de las clases esta optimzada aun.

---
+++ Heat_1_2_3_MAT_D.cxx
@@ -0,0 +1,26 @@
+
+/**
+ * \file Heat1D
+ *
+ *
+ * \author Jorge
+ *
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+#include "Vector.h"
+#include "Matrix.h"
+#include "Heat1D.h"
+#include "Heat2D.h"
+
+int main()
+{
+Heat1D(0.3125,3,0.1);
+Heat2D(0.3125,3,0.1);
+return 0;
+
+//transpose vector
+}
---
+++ Matrix.h
@@ -0,0 +1,123 @@
+#ifndef MATRIX_H_INCLUDED
+#define MATRIX_H_INCLUDED
+/**
+ * \file vector
+ *
+ *
+ * \author Ronald Cumbal
+ *
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <array>
+#include <iomanip>
+#include <cmath>
+
+#include "Vector.h"
+
+
+template<typename T>
+class Matrix
+{
+public:
+    //General parameters
+    int print_space = 5;
+    int decimal_space = 1;
+
+    //Default Constructor
+    Matrix(): row(0), col(0){}
+
+    //Constructor that takes dimensions
+    Matrix(std::size_t r, std::size_t c): row(r), col(c){}
+    //Assign values to Matrix
+
+    T& operator[](std::array<int,2> arr)
+    {
+        if (arr[0] >= row || arr[1] >= col )
+            std::cout << "Element out of size!!" <<std::endl;
+        return data[{arr[0],arr[1]}];
+    }
+
+    T operator[](std::array<int,2> arr) const
+    {
+        if (arr[0] >= row || arr[1] >= col )
+            std::cout << "Element out of size!!" <<std::endl;
+        return data[{arr[0],arr[1]}];
+    }
+
+    Vector<T> matvec(const Vector<T>& vec)
+    {
+        if (vec.size()!=row)
+        {
+            std::cout<< "Matrix and Vector don't match size for multiplication" << std::endl;
+            return 0;
+        }
+        else
+        {
+            Vector<T> result(vec.size());
+            //set result to zero first !!!!!!!!!!!!!
+            for (int r=0; r<row;r++)
+                for (int c=0; c<col;c++)
+                    result.get(r)+=data[{r,c}]*vec.get(c);
+            return result;
+        }
+    }
+
+    //Help to get private attributes in out of scope functions
+    inline const std::size_t get_row() const { return row; }
+    inline const std::size_t get_col() const { return col; }
+
+    void print()
+    {
+        for (int r=0; r<row;r++)
+        {
+            for (int c=0; c<col;c++)
+            {
+                if (c==0) {std::cout << "[";}
+                if (data[{r,c}]==0)
+                    {std::cout <<std::setw(print_space)<<" ";}
+                else
+                    {std::cout <<std::setw(print_space)<<std::setprecision(decimal_space)<<std::fixed<<data[{r,c}];}
+                if (c==col-1) {std::cout << "]";}
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void print_zeros()
+    {
+        for (int r=0; r<row;r++)
+        {
+            for (int c=0; c<col;c++)
+            {
+                if (c==0) {std::cout << "[";}
+                std::cout <<std::setw(print_space)<<std::setprecision(decimal_space)<<std::fixed<<data[{r,c}];
+                if (c==col-1) {std::cout << "]";}
+            }
+            std::cout << std::endl;
+        }
+    }
+
+		// Transpose
+	Matrix<T> transpose()
+	{
+		Matrix<T> Transp(get_col(),get_row()) ;
+		for (int r=0; r<row;r++)
+        {
+            for (int c=0; c<col;c++)
+            {
+				Transp[{c,r}]=data[{r,c}];
+            };
+        };
+		return Transp;
+	};
+
+private:
+    const std::size_t row;
+    const std::size_t col;
+    std::map<std::array<int,2>,T> data;
+};
+
+#endif // MATRIX_H_INCLUDED
