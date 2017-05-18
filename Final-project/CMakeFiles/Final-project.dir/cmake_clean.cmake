cea386c2e24ab0fe97cffba5b35022fa50ac5bfd
Jorge <j.fuchsilloldi@student.tudelft.nl>
2017-05-18 18:52:52 +0200

    con log

---
+++ Final-project/CMakeFiles/Final-project.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Final-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Final-project/CMakeFiles/Final_project_assignment.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Final_project_assignment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Final_project_assignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
