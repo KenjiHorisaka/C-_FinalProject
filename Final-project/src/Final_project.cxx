dfef36f92192d0fdbe78b017ebad0651a3004938
ronaldcumbal <ronald.cumbal.g@gmail.com>
2017-05-22 09:13:39 +0200

    Forgot to uncomment some part of the code ;)

--- Final-project/src/Final_project.cxx
+++ Final-project/src/Final_project.cxx
@@ -21,7 +21,7 @@ int main(){
 	clock_t t1,t2;
 	double t_e;
 /* ********************** 1D Template Solution************* */
-/*	std::cout<<"\n|-------TEMPLATE SOLUTION FOR 1D-------|"<<std::endl;
+	std::cout<<"\n|-------TEMPLATE SOLUTION FOR 1D-------|"<<std::endl;
 	std::cout<<"\nMATRIX M   m=3 dt=0.1"<<std::endl;
 	Heat<1>(0.3125,3,0.1).printM();
 	t1=clock();
@@ -40,7 +40,7 @@ int main(){
 	std::cout<<t_e<<" sec"<<std::endl;
 
 /* ********************** 2D Template Solution************* */
-/*	std::cout<<"\n|-------TEMPLATE SOLUTION FOR 2D-------|"<<std::endl;
+	std::cout<<"\n|-------TEMPLATE SOLUTION FOR 2D-------|"<<std::endl;
 	std::cout<<"\nMATRIX M   m=3 dt=0.1"<<std::endl;
 	Heat<2>(0.3125,3,0.1).printM();
 	t1=clock();
@@ -65,10 +65,10 @@ int main(){
 	Heat<3>(0.3125,3,0.1).printM();
 	t1=clock();
 	std::cout<<"\nTHIS IS THE EXACT SOLUTION FOR m=30 dt=0.001 tf=0.5 "<<std::endl;
-	Vector<double> exact3 = Heat<3>(0.3125,5,0.001).exact(0.5);
+	Vector<double> exact3 = Heat<3>(0.3125,30,0.001).exact(0.5);
 	//exact3.print();
 	std::cout<<"\nTHIS IS THE NUMERICAL SOLUTION FOR m=30 dt=0.001 tf=0.5 "<<std::endl;
-	Vector<double> solve3 = Heat<3>(0.3125,5,0.001).solve(0.5);
+	Vector<double> solve3 = Heat<3>(0.3125,30,0.001).solve(0.5);
 	//solve3.print();
 	std::cout<<"\nMEAN ERROR OF SOLUTIONS"<<std::endl;
 	Vector <double> error3=exact3-solve3;
